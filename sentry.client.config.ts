70de9b89d7a171f8b673eba184bdd279f02436b8
Vitals9367 <vitalijus.alsauskas@gmail.com>
2025-01-12 12:39:26 +0200

    build

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -5,7 +5,7 @@
 import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
-  dsn: process.env.SENTRY_DSN,
+  dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
   environment: process.env.SENTRY_ENVIRONMENT || 'development',
   // Add optional integrations for additional features
   integrations: [
